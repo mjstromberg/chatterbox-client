3b35c44d9b157660604ff988a461f8320b00b62f
mjstromberg <michaeljstromberg@gmail.com>
2016-09-26 20:16:57 -0700

    Implement room selection functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,27 @@
 // store time stamp of last chat
 var lastChatTimeStamp = 0;
+var rooms = {};
+var currentRoom = 'Lobby';
+
 
 var getChats = function(data) {
-  console.log(data);
-  var lastNewChatFound = false;
-
   // loop through each item in data.results
   data.results.forEach(function(obj) {
-    // append each chat to the DOM in a div with a class of chats
+    // if new room, add to select element
+    if (!rooms.hasOwnProperty(obj.roomname)) {
+      rooms[obj.roomname] = true;
+      $('#roomSelect').append('<option>' + obj.roomname + '</option>');
+    }
 
+    console.log(currentRoom);
+    // append each chat to the DOM in a div with a class of chats
     //sample timestamp: "2016-09-26T22:17:29.693Z"
     if (Date.parse(obj.createdAt) > lastChatTimeStamp) {
-      var nameDiv = '<div class="username">@' + obj.username + '</div>';
-      var textDiv = '<div class="chat">' + obj.text + '</div>';
-      $('#chats').append('<div class="chat">' + nameDiv + textDiv + '</div>');
+      if (currentRoom === 'Lobby') {
+        app.renderMessage(obj);
+      } else if (obj.roomname === currentRoom) {
+        app.renderMessage(obj);
+      }
     }
   });
 
@@ -21,39 +29,23 @@ var getChats = function(data) {
   lastChatTimeStamp = Date.parse(data.results[0].createdAt);
 };
 
-// GET requests
-setInterval(function() {
-  $.get('https://api.parse.com/1/classes/messages', getChats);
-}, 1000);
+var app = {};
 
-// POST requests
-// On submit button click
-  // Example:
-  // $("button").click(function(){
-    // $.post("demo_test_post.asp",
-    // {
-    //   name: "Donald Duck",
-    //   city: "Duckburg"
-    // },
-    // function(data,status){
-    //     alert("Data: " + data + "\nStatus: " + status);
-    // });
-  // });
+app.server = 'https://api.parse.com/1/classes/messages';
 
-// helper url parser for username
-var usernameParser = function(parameters) {
-  var index = parameters.indexOf('username=');
-  return parameters.slice(index + 9);
-};
+app.init = function() {};
 
-var postChat = function() {
+app.send = function(data) {
+  // clear chatroom
+  app.clearMessages;
+
   // Grab the input node and extract the input value
   var inputText = $('input[name="chatInput"]').val();
   // POST
-  var data = {
+  data = data || {
     username: usernameParser(window.location.search),
     text: inputText,
-    roomname: 'blank'
+    roomname: currentRoom
   };
   // $.post('https://api.parse.com/1/classes/messages', data, function() { alert('posted'); });
   $.ajax({
@@ -61,23 +53,86 @@ var postChat = function() {
     type: 'POST',
     data: JSON.stringify(data),
     contentType: 'application/json',
-    success: function() { alert('posted'); }
+    success: function() { console.log('posted'); }
   });
+
+  alert('created new chat: ' + currentRoom);
+  
+  //reset currentRoom
+
+  // re-fetch the updated database
+  app.fetch();
 };
+
+app.fetch = function() {
+  // get request
+  $.get('https://api.parse.com/1/classes/messages', getChats);
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.renderMessage = function(dataObject) {
+  var nameDiv = '<div class="username">@' + dataObject.username + '</div>';
+  var textDiv = '<div class="chat">' + dataObject.text + '</div>';
+  // var roomDiv = '<div class="chat>' + dataObject.roomname + '</div>';
+  $('#chats').append('<div class="chat">' + nameDiv + textDiv + '</div>');
+};
+
+app.renderRoom = function(roomName) {
+  $('#roomSelect').append('<option value=' + roomName + '>' + roomName + '</option>');
+};
+
+// // GET requests
+setInterval(function() {
+  app.fetch();
+}, 1000);
+
+// helper url parser for username
+var usernameParser = function(parameters) {
+  var index = parameters.indexOf('username=');
+  return parameters.slice(index + 9);
+};
+
+var roomChange = function() {
+  var text = $('#roomSelect option:selected').text();
+
+  if (text === 'Create New Room') {
+    result = window.prompt('Input new chat room name:', 'Ex: Panic Room');
+    app.renderRoom(result);
+    currentRoom = result;
+    alert('created new room: ' + currentRoom);
+  } else {
+    currentRoom = text;
+  }
+
+  app.clearMessages();
+  lastChatTimeStamp = 0;
+  app.fetch();
+};
+
 
 /* PLAN
 
-x Build a div to hold all of our chats
-- Build structure to get, post, put, etc. to the server
-  x Build ajax get request using jquery
-  x Append chats to our chats div
-  x Set up an interval to grab new chats from the server/database
-  x Build HTML for our form input field
-  x Build a submit button
-  x Define callback function for ajax post request
-  x Build ajax post request using jquery
-  x Set up button and on click functionality to post the post request
-- Build security checks
+- Implement room functionality
+  x Populate select element with current rooms
+  - Add create room functionality
+    x Add room to database of rooms
+    - After creating new room, switch selected room to the new room name
+    - After creating new room, point 'select' to the correct 'option' (maybe)
+    x Update POSTs to have the correct roomname
+  x Filter chats by room property
+    x Set a global variable called currentRoom (default is Lobby)
+    x Refactor getChats to filter for the proper room
+    x On roomChange
+      x clearMessages
+      x set currentRoom to text
+      x reset lastChatTimeStamp
+      x call app.fetch
+- Create our XSS attack filter thingy
+- Add friend
+- Send message upon clicking submit
 
 
 */
